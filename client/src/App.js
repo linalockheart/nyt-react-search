88386e238fc7c654e366272c66685af9d514c52c
Lina Lockheart <angelinalockheart@gmail.com>
2018-09-28 22:38:32 -0400

    imported components

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <Header/>
+        <Header/>
         <Wrapper>
           <Search>
           </Search>
@@ -18,7 +18,7 @@ class App extends Component {
           </Results>
           <Saved>
           </Saved>
-        </Wrapper> */}
+        </Wrapper>
       </div>
     );
   }
