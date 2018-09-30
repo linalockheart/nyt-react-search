f6120ff87bce6a89d59843b0c758907bd95a3214
Lina Lockheart <angelinalockheart@gmail.com>
2018-09-30 13:29:23 -0400

    added utils

---
+++ client/src/utils/API.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default {
+  searchArticles: function(query) {
+    return axios.get("/api/key")
+        .then(res => axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {params: { "api-key": res.data.apiKey, "q": query}}))
+        .catch(err => console.log(err));
+  },
+  // Gets all articles
+  getArticles: function() {
+    return axios.get("/api/articles");
+  },
+  // Gets the article with the given id
+  getArticle: function(id) {
+    return axios.get("/api/articles/" + id);
+  },
+  // Deletes the article with the given id
+  deleteArticle: function(id) {
+    return axios.delete("/api/articles/" + id);
+  },
+  // Saves a article to the database
+  newArticle: function(articleData) {
+    return axios.post("/api/articles", articleData);
+  }
+};
