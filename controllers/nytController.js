be5b54fe10336b62a092c2242c9ae41a6d9543ed
Lina Lockheart <angelinalockheart@gmail.com>
2018-10-02 20:58:17 -0400

    nyt controller complete

--- controllers/nytController.js
+++ controllers/nytController.js
@@ -12,6 +12,19 @@ module.exports = {
             req.query
         );
         axios
-          .get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {params}
-        })
-}
+          .get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key=", {params})
+          .then(response => {
+            db.Article
+              .find()
+              .then(dbArticles =>
+                response.data.response.docs.filter(articles => 
+                  dbArticles.every(
+                    dbArticle => dbArticle._id.toString() !== article._id
+                  )
+                )
+              )
+              .then(articles => res.json(articles))
+              .catch(err => res.status(422).json(err));
+          });
+    }
+};
